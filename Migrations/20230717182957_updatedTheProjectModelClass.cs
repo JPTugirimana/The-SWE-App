2beb6af1cd59d9deb9d8e4391aa206eb56e97531
JP Tugirimana <tugirimana.jp@gmail.com>
2023-07-17 13:18:47 -0700

    Modified Project Model class to validate links and added unit tests for the link properties.

---
+++ Migrations/20230717182957_updatedTheProjectModelClass.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sweapp.Migrations
+{
+    public partial class updatedTheProjectModelClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_AspNetUsers_AppUserId",
+                table: "Project");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Project_AppUserId",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GithubUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkedInUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LinkUrl",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AppUserId",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LinkUrl",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AppUserId",
+                table: "Project",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GithubUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedInUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_AppUserId",
+                table: "Project",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_AspNetUsers_AppUserId",
+                table: "Project",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
--- Models/Project.cs
+++ Models/Project.cs
@@ -11,10 +11,43 @@ namespace sweapp.Models
         [Required]
         public string Name { get; set; }
         public string? Description { get; set; }
-        public string? LinkUrl { get; set; }
-        public string? ImageUrl { get; set;}
+        private string? linkUrl;
+        private string? imageUrl;
 
         [ForeignKey("AppUser")]
         public string? AppUserId { get; set;}
+
+        [Display(Name = "Project Link")]
+        public string LinkUrl
+        {
+            get { return linkUrl; }
+
+            set
+            {
+                if (IsValidUrl(value))
+                    linkUrl = value;
+                else
+                    linkUrl = "Link NOT provided!";
+            }
+        }
+
+        public string ImageUrl
+        {
+            get { return imageUrl; }
+
+            set
+            {
+                if (IsValidUrl(value))
+                    imageUrl = value;
+                else
+                    imageUrl = "Link NOT provided!";
+            }
+        }
+
+        public static bool IsValidUrl(string url)
+        {
+            return Uri.IsWellFormedUriString(url, UriKind.Absolute);
+        }
+
     }
 }
