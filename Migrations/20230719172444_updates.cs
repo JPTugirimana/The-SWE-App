878941cd2d4cc823d95e0f025058c6940ded470f
JP Tugirimana <tugirimana.jp@gmail.com>
2023-07-19 10:28:01 -0700

    updated the gitignore

---
+++ Migrations/20230719172444_updates.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sweapp.Migrations
+{
+    public partial class updates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
